e95acd40633077cf2efdf3c1afcc1ba651206b33
Neon <wiseearl@gmail.com>
2024-06-13 10:03:57 +0800

    Init

---
+++ nodejs-html-sample/server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading file');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end('Page not found');
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ readme.md
@@ -0,0 +1,9 @@
+mkdir nodejs-html-sample
+cd nodejs-html-sample
+
+npm init -y
+
+add file: server.js
+add file: index.html
+
+run cmd : node server.js
